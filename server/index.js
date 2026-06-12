a2773516026cc109b84e605f5fe373be6f6e7cfa
Swarn Pratap Singh <swarnpratapsingh5@gmail.com>
2026-06-12 15:59:46 -0400

    fix: add vercel domain to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'https://the-bridge-dusky.vercel.app',
     'https://thebridge.bridgemakersmn.org',
   ],
   credentials: true,
